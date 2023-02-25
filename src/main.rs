71950e32e240731bd1e45ffa508f009b7f08af4e
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2023-02-25 23:38:11 +0100

    [WIP]

--- src/main.rs
+++ src/main.rs
@@ -15,44 +15,44 @@ async fn index() -> impl Responder {
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
     println!("Starting the server at 0.0.0.0:9090");
-
+    
+    env_logger::init();
     env::set_var("RUST_LOG", "actix_web=debug,actix_server=debug");
 
     domain::services::poll_service::poll(20);
 
 
-    let res = 
-        reqwest::get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=NVDA&apikey=5580KYOVDAQFS7CJ")
-            .await;
+    // let res = 
+    //     reqwest::get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=NVDA&apikey=5580KYOVDAQFS7CJ")
+    //         .await;
     
-    match res {
-        Ok(res) => {
-            println!("res: {:?}", res);
-            match res.json::<domain::records::vantage_records::TimeSeriesDailyAdjusted>().await {
-                Ok(text) => {
-                    println!("Symbol: {:?}", text.meta_data.symbol);
-                    ;
-                    // println!("{}", text.time_series_daily_adjusted.as_object().unwrap().keys().collect::<Vec<_>>()[0]);
-                    for (key, value) in text.time_series_daily_adjusted.as_object().unwrap().iter() {
-                        println!("\x1b[93mError\x1b[0m");
-                        println!("{}: {}", key, value.as_object().unwrap().get("1. open").unwrap());
-                    }
-                    
-                },
-                Err(err) => {
-                    println!("err: {:?}", err);
-                }
-            }
-        },
-        Err(err) => {
-            println!("err: {:?}", err);
-        }
-    }
+    // match res {
+    //     Ok(res) => {
+    //         println!("res: {:?}", res);
+    //         match res.json::<domain::records::vantage_records::TimeSeriesDailyAdjusted>().await {
+    //             Ok(text) => {
+    //                 println!("Symbol: {:?}", text.meta_data.symbol);
+    //                 // println!("{}", text.time_series_daily_adjusted.as_object().unwrap().keys().collect::<Vec<_>>()[0]);
+    //                 for (key, value) in text.time_series_daily_adjusted.as_object().unwrap().iter() {
+    //                     println!("\x1b[93mError\x1b[0m");
+    //                     println!("{}: {}", key, value.as_object().unwrap().get("1. open").unwrap());
+    //                 }
+
+    //             },
+    //             Err(err) => {
+    //                 println!("err: {:?}", err);
+    //             }
+    //         }
+    //     },
+    //     Err(err) => {
+    //         println!("err: {:?}", err);
+    //     }
+    // }
 
 
 
 
-    env_logger::init();
+    
     HttpServer::new(|| {
         App::new()
             // enable logger - always register actix-web Logger middleware last
